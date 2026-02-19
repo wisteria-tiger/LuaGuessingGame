8c98c8d358c79069753e8cf49ab16d78f82e1422
wisteria-tiger <dartingsparrow77@icloud.com>
2026-02-18 19:44:28 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@
 -- Purpose: Simulate a guessing game to guess a number between 1 and 100 in 7 tries
 
 while true do
-    io.write("Do you want to play a guessing game? (yes/no)")
+    io.write("Do you want to play a guessing game? (yes/no): ")
     local userChoice = string.lower(io.read())
     
     if userChoice == "yes" then
@@ -10,6 +10,7 @@ while true do
         math.random(); math.random(); math.random();
         local random_number = math.random(1, 100)
         print("I'm thinking of random number between 1 and 100. Can you guess it in 7 tries?")
+
         for x = 1, 7 do
             io.write("Guess a number: ")
             local userGuess = tonumber(io.read())
@@ -18,17 +19,17 @@ while true do
                 io.write("Not a valid input. Try again.")
                 userGuess = tonumber(io.read())
             end
-
+            
             if userGuess == random_number then
                 print("You guessed right! Congratulations!")
                 break
             elseif userGuess > random_number then
-                print("You guessed too high. try again.")
+                print("You guessed too high. Try again.")
             elseif userGuess < random_number then
                 print("You guessed too low. Try again.")
             end
                 
-            if userGuess ~= random_number and x == 7 then
+            if userGuess ~= random_number and x == 6 then
                 print("Sorry, you lose! the number was: " .. random_number)
             end
         end
@@ -36,5 +37,4 @@ while true do
         print("Bye!")
         break
     end
-    
 end
